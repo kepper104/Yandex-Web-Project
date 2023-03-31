f8074bf6c898f3f54e28cdf98b21e4235e99ebd1
kepper104 <67190416+kepper104@users.noreply.github.com>
2023-03-31 17:37:04 +0300

    Add Local Hosting

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, redirect
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
@@ -66,4 +68,5 @@ def make_post():
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
