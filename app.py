313f74c19ff37ae20c9684c2769c142cc8581e2e
kepper104 <67190416+kepper104@users.noreply.github.com>
2023-04-03 12:21:59 +0300

    a

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ def post(post_id):
 @flask_login.login_required
 def make_post():
     if request.method == "GET":
-        return render_template(url_for("makepost"))
+        return render_template(url_for("make_post"))
     print(request.form)
 
     commit_post(request.form)
